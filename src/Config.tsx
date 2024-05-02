86cd318c3cbc03ccf86f7c3dcea1adf8355a351d
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-05-02 13:47:04 +0530

    fix: secrets

--- src/Config.tsx
+++ src/Config.tsx
@@ -24,17 +24,13 @@ import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
-
-// console.log(typeof import.meta.env.VITE_API_KEY);
-// let hell = "hell";
-// console.log(hell);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
